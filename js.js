51ba9645916f92bc2ba607d118877f45b9bdc5f8
Pratistha <pratisthashrestha03@gmail.com>
2021-10-28 22:21:48 +0545

    base apparel coming soon master completed

---
+++ js.js
@@ -0,0 +1,3 @@
+let C1= 4;
+let C2=9;
+let sum= C1+C2;
--- p4/validateemail.js
+++ p4/validateemail.js
@@ -1,20 +1,22 @@
-document.write('<h1>i am js</h1>');
 function valid(){
     var form = document.getElementById("form");
     var email = document.getElementById("email").value;
     var text = document.getElementById("text");
+    let errorImg = document.getElementById("error");
     var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;//for email validation
 
     if(email.match(pattern)){
         form.classList.add("valid");
         form.classList.remove("invalid");
         text.innerHTML="You have successfully registered."
+        errorImg.style.visibility = "hidden";
         text.style.color="#00ff00";
     }
     else{
         form.classList.remove("valid");
         form.classList.add("invalid");
         text.innerHTML="Please provide a valid email";
+        errorImg.style.visibility = "visible";
         text.style.color="#ff0000";
     }
     if(email= ""){
