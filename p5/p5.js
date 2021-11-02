38d53df86462820e37bc0cc2056f088cf9a50ffc
Pratistha <pratisthashrestha03@gmail.com>
2021-11-02 22:14:17 +0545

    toggle master layout completed

--- p5/p5.js
+++ p5/p5.js
@@ -17,42 +17,33 @@ function valid(){
     let emailimgerror=document.getElementById("emailerrorimg");
     let pwimgerror=document.getElementById("pwerrorimg");
 
-
-
     if(email.match(pattern)){
-        form.classList.add("valid");
-        form.classList.remove("invalid");
         emailimgerror.style.visibility = "hidden";
     }
     else{
-      form.classList.remove("valid");
-      form.classList.add("invalid");
-      emailimgerror.style.visibility = "visible";
+    emailimgerror.style.visibility = "visible";
       emailerror.style.visibility="visible";
     }
     
-    if(fname== " "){
-    //   form.classList.remove("valid");
-    //   form.classList.add("invalid");
-      fnameimgerror.style.visibility = "visible";
-      fnameerror.innerHTML="First name abt be empty.";          
-  }
-    if(lname== " "){
-    //   form.classList.remove("valid");
-    //   form.classList.remove("invalid");
-      lnameimgerror.style.visibility = "visible";
-      lnameerror.style.visibility="visible";
+    if(fname= " "){
+    fnameimgerror.style.visibility = "visible";
+    fnameerror.style.visibility="visible";  
     }
-    // if(email== null){
-    //   form.classList.remove("valid");
-    //   form.classList.remove("invalid");            emailimgerror.style.visibility = "visible";
-    //   emailimgerror.style.visibility = "visible";
-    //   emailerror.style.visibility="visible";
+    // else{
+    //     fnameimgerror.style.visibility = "hidden";
+    //     fnameerror.style.visibility="hidden"; 
     // }
-    if(password.length<8 ){
-    //form.classList.remove("valid");
-    //form.classList.remove("invalid");
-      passworderror.style.visibility="visible";
+    if(lname= " "){
+    lnameimgerror.style.visibility = "visible";
+    lnameerror.style.visibility="visible";
+  }
+//   else{
+//     lnameimgerror.style.visibility = "hidden";
+//     lnameerror.style.visibility="hidden";
+//   }
+    
+    if(password.length < 8 ){
+        passworderror.style.visibility="visible";
       pwimgerror.style.visibility = "visible";
     }
     
